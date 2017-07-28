55040ffb7d39924a2157e98ea49f478bb3a79f9a
Ip <jun040329@gmail.com>
2017-07-29 08:50:00 +0900

    Update PocketMine.php

--- src/pocketmine/PocketMine.php
+++ src/pocketmine/PocketMine.php
@@ -75,7 +75,7 @@ namespace pocketmine {
 
 	const VERSION = "1.2.0";
 	const API_VERSION = "3.0.1";
-	const CODENAME = "Ruby";
+	const CODENAME = "ServerSoftwareArchiveTeam";
 
 	/*
 	 * Startup code. Do not look at it, it may harm you.
